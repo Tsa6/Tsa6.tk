bc6eee382703a0aa663d0bb86eadf75d7fd86881
Taizo Simpson <TaizoSimpson@icloud.com>
2017-07-05 18:25:00 -0400

    Added tsa6.tk and the rest of herokuapp.com to allowed hosts

--- tsa6tk/settings.py
+++ tsa6tk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4w$!#_np$7(c(&vr=5v3b7etjsp&-p!q=iv#$%+&(o+!$w75rz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','tsa6tk.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','.herokuapp.com','.tsa6.tk']
 
 
 # Application definition
