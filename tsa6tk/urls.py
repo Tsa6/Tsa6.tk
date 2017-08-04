72b7f69f9f38159a6d5f1145b08f159e8c74fabb
Taizo 'Tsa6' Simpson <taizo@tsa6.tk>
2017-08-04 13:43:48 -0400

    Removed database

--- tsa6tk/urls.py
+++ tsa6tk/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
+from tsa6tk.errors import handle404
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^(?:webapps/)?mlptkeps/?', include('mlptkeps.urls'))
 ]
 
